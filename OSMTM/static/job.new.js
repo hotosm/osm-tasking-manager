19eaeab016acaff5f1de55aa3dbeca486566078e
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2011-10-22 14:42:17 +0200

    Tell the admin how many tiles can be created with the given geometry and zoom level

--- OSMTM/static/job.new.js
+++ OSMTM/static/job.new.js
@@ -1,5 +1,5 @@
 var map = null,
-    boxLayer = null,
+    vectorLayer = null,
     imageryLayer = null,
     tiles = null;
 
@@ -35,17 +35,19 @@ function plotBox (bounds) {
     var mercBounds = new OpenLayers.Bounds();
     mercBounds.extend(bounds);
     mercBounds.transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject());
-    boxLayer.clearMarkers()
-    boxLayer.addMarker(new OpenLayers.Marker.Box(mercBounds));
-    $('#geometry')[0].value = mercBounds.toGeometry();
+    vectorLayer.removeFeatures(vectorLayer.features);
+    var geometry = mercBounds.toGeometry();
+    vectorLayer.addFeatures([new OpenLayers.Feature.Vector(geometry)]);
+    $('#geometry').val(geometry);
+    adaptZoomLevel(mercBounds);
     $('#bbox').val(bounds);
     $('#id_submit')[0].disabled = false;
 }
 
 function showBoundingBoxMap () {
     $('#relation_loading_msg').hide();
-    boxLayer = new OpenLayers.Layer.Boxes("BBox");
-    map.addLayer(boxLayer);
+    vectorLayer = new OpenLayers.Layer.Vector("BBox");
+    map.addLayer(vectorLayer);
     var control = new OpenLayers.Control();
     OpenLayers.Util.extend(control, {
         draw: function () {
@@ -87,6 +89,19 @@ function plotBoxFromInput() {
     plotBox(bounds);
 }
 
+// set the zoom level to the more appropriate value
+function adaptZoomLevel(bounds) {
+    var zoom = $('#id_zoom').val(),
+        res = map.getResolutionForZoom(zoom),
+        // the size of a tile in meters for the given zoom 
+        tileSize = res * 256,
+        // the number of tile in a row
+        nbByRow = Math.abs(bounds.right - bounds.left) / tileSize,
+        nbByCol = Math.abs(bounds.top - bounds.bottom) / tileSize;
+    $('#zoom_level_info').show();
+    $('#nb_tiles').html(Math.round(nbByRow * nbByCol));
+}
+
 $('input[name=relation_type]')
     .change(function() {
         resetMap();
@@ -106,7 +121,7 @@ $('#id_relation')
         if ($("input[name=relation_type]").val() == "relation") {
             $('#relation_loading_msg').show();
             var url = "http://www.openstreetmap.org/api/0.6/relation/" + this.value + '/full';
-            var layer = new OpenLayers.Layer.GML("Objects", url, {
+            vectorLayer = new OpenLayers.Layer.GML("Objects", url, {
                 format: OpenLayers.Format.WKT,
                 style: {
                     strokeColor: "blue",
@@ -120,20 +135,22 @@ $('#id_relation')
                 displayInLayerSwitcher: false
             });
 
-            layer.events.register("loadend", layer, function() {
+            vectorLayer.events.register("loadend", vectorLayer, function() {
                 $('#relation_loading_msg').hide();
-                map.zoomToExtent(layer.getDataExtent());
+                map.zoomToExtent(vectorLayer.getDataExtent());
                 var format = new OpenLayers.Format.WKT();
-                $('#geometry')[0].value = format.write(layer.features[0]);
+                $('#geometry').val(format.write(vectorLayer.features[0]));
+                adaptZoomLevel(vectorLayer.getDataExtent());
                 $('#id_submit')[0].disabled = false;
             });
 
-            map.addLayer(layer);
-            layer.loadGML();
+            map.addLayer(vectorLayer);
+            vectorLayer.loadGML();
         } else if ($('#bbox').val() != '') {
             plotBoxFromInput();
         }
     });
+
 
 $('#id_imagery')
     .change(function() {
@@ -160,8 +177,7 @@ $('#id_imagery_toggle')
 
 $('#id_zoom')
     .change(function() {
-        //tiles.zoom = this.value; 
-        //tiles.redraw();
+        adaptZoomLevel(vectorLayer.getDataExtent());
     });
 
 
--- OSMTM/templates/job.new.mako
+++ OSMTM/templates/job.new.mako
@@ -40,17 +40,22 @@
                     <div class="clearfix">
                         <label for="id_zoom">Zoom level</label> 
                         <div class="input">
-                        <select id="id_zoom" name="zoom">
-                             <option>10</option>
-                             <option>11</option>
-                             <option selected="selected">12</option>
-                             <option>13</option>
-                             <option>14</option>
-                             <option>15</option>
-                             <option>16</option>
-                             <option>17</option>
-                             <option>18</option>
-                        </select>
+                            <select id="id_zoom" name="zoom">
+                                 <option>10</option>
+                                 <option>11</option>
+                                 <option selected="selected">12</option>
+                                 <option>13</option>
+                                 <option>14</option>
+                                 <option>15</option>
+                                 <option>16</option>
+                                 <option>17</option>
+                                 <option>18</option>
+                            </select>
+                            <span class="help-block">
+                                <span id="zoom_level_info">
+                                    Up to <strong id="nb_tiles"></strong> tiles will be created.
+                                </span>
+                            </span>
                         </div> 
                     </div>
                     <div class="clearfix">
