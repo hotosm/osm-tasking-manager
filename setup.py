24eda529203ddc7cacc2248b902ed86abb2a907b
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2012-03-23 18:01:55 +0100

    We want pyramid 1.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README = open(os.path.join(here, 'README.txt')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires = [
-    'pyramid>1.2',
+    'pyramid>1.2.99',
     'pyramid_beaker',
     'SQLAlchemy',
     'Shapely',
