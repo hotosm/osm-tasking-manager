18f664237f6a4c1885d38a2094e87c0b39125be0
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2014-02-09 13:47:05 +0100

    Prep for 0.1

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ if sys.version_info[:3] < (2,5,0):
     requires.append('pysqlite')
 
 setup(name='OSMTM',
-      version='0.0',
+      version='0.1',
       description='OSMTM',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
